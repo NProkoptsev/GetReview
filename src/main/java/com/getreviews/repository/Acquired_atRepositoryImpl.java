fc79e07b79ad40de375c10b50c329ea23adda087
nprok <x4595k4d>
2016-11-29 18:21:16 +0300

    acquired_at impl

--- src/main/java/com/getreviews/repository/Acquired_atRepositoryImpl.java
+++ src/main/java/com/getreviews/repository/Acquired_atRepositoryImpl.java
@@ -1,14 +1,69 @@
 package com.getreviews.repository;
 
-import com.getreviews.domain.Acquired_at;
+import com.getreviews.domain.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
-/**
- * Created by grigorijpogorelov on 29.11.16.
- */
+import java.sql.*;
+import java.util.List;
+
 public class Acquired_atRepositoryImpl implements Acquired_atRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    private RowMapper<Acquired_at> rowMapper = new RowMapper<Acquired_at>() {
+
+        @Override
+        public Acquired_at mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Acquired_at acquired_at = new Acquired_at();
+            acquired_at.setId(rs.getLong("id"));
+            acquired_at.setPrice(rs.getFloat("price"));
+            Item item = new Item();
+            item.setId(rs.getLong("item_id"));
+            acquired_at.setItem(item);
+            Shop shop = new Shop();
+            shop.setId(rs.getLong("shop_id"));
+            acquired_at.setShop(shop);
+            return acquired_at;
+        }
+    };
+
     @Override
     public <S extends Acquired_at> S save(S entity) {
-        return null;
+        String sql;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        final Long id = entity.getId();
+
+        if (id == null) {
+            sql =
+                "insert into acquired_at (price, item_id, shop_id) values (?, ?, ?)";
+        } else {
+            sql =
+                "update acquired_at set price = ?, item_id = ?, shop_id = ? where id = ?";
+        }
+
+        PreparedStatementCreator psCreator =
+            new PreparedStatementCreator() {
+                @Override
+                public PreparedStatement createPreparedStatement(
+                    Connection con) throws SQLException {
+                    PreparedStatement ps = con.prepareStatement(
+                        sql, Statement.RETURN_GENERATED_KEYS);
+                    ps.setFloat(1, entity.getPrice());
+                    ps.setLong(2, entity.getItem().getId());
+                    ps.setLong(3, entity.getShop().getId());
+                    if (id != null) ps.setLong(4, entity.getId());
+                    return ps;
+                }
+            };
+
+        jdbcTemplate.update(psCreator, keyHolder);
+
+        entity.setId((long) keyHolder.getKeys().get("id"));
+        return entity;
     }
 
     @Override
@@ -18,7 +73,9 @@ public class Acquired_atRepositoryImpl implements Acquired_atRepository {
 
     @Override
     public Acquired_at findOne(Long aLong) {
-        return null;
+        Acquired_at acquired_at = jdbcTemplate.queryForObject("select id, price, item_id, shop_id from acquired_at WHERE id=?",
+            new Object[]{aLong}, rowMapper);
+        return acquired_at;
     }
 
     @Override
@@ -28,7 +85,8 @@ public class Acquired_atRepositoryImpl implements Acquired_atRepository {
 
     @Override
     public Iterable<Acquired_at> findAll() {
-        return null;
+        List<Acquired_at> acquired_ats = jdbcTemplate.query("select id, price, item_id, shop_id from acquired_at", rowMapper);
+        return acquired_ats;
     }
 
     @Override
@@ -38,17 +96,17 @@ public class Acquired_atRepositoryImpl implements Acquired_atRepository {
 
     @Override
     public long count() {
-        return 0;
+        return jdbcTemplate.queryForObject("select count(*) from acquired_at", Long.class);
     }
 
     @Override
     public void delete(Long aLong) {
-
+        jdbcTemplate.update("delete from acquired_at where id = ?", aLong);
     }
 
     @Override
     public void delete(Acquired_at entity) {
-
+        jdbcTemplate.update("delete from acquired_at where id = ?", entity.getId());
     }
 
     @Override
@@ -58,6 +116,6 @@ public class Acquired_atRepositoryImpl implements Acquired_atRepository {
 
     @Override
     public void deleteAll() {
-
+        jdbcTemplate.update("delete from acquired_at");
     }
 }
